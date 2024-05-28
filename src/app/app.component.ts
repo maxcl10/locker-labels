7c8e8ae9b89b83c5ffc75335bd7729dd177d48f3
Matter Maxime <maxime.matter@swiss.com>
2024-05-28 15:06:33 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, computed, input, model, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'case-labels';
+
+  name = signal<string>('');
+  fourLetterCode = computed(()=>
+  {
+    const items = this.name().split('/');
+    return items[items.length - 1];
+  });
+  division =  computed(()=>
+    {
+      const items = this.name().split('/');
+      return items.slice(0, -1).join('/') + '/';
+    });;
+
+  constructor() {
+
+  }
 }
