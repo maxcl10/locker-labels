0da7caafc777527d1d5a74454fa69da25357156b
Matter Maxime <maxime.matter@swiss.com>
2024-05-30 10:24:49 +0200

    change size depending on length

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import {Component, computed, signal} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {RouterOutlet} from '@angular/router';
+import {NgStyle} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule, NgStyle],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -13,10 +14,22 @@ export class AppComponent {
   title = 'case-labels';
 
   name = signal<string>('BSLS/IEG/TEST');
+
   fourLetterCode = computed(() => {
     const items = this.name().split('/');
     return items[items.length - 1];
   });
+
+  fontSize = computed(() => {
+    const length = this.name().length;
+    if (length > 10) {
+      return 28
+    } else if (length > 4) {
+      return 34
+    } else {
+      return 40;
+    }
+  })
 
   division = computed(() => {
     const items = this.name().split('/');
